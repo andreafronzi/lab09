93b25c1c1d2687e5091eb03b1da8a29d67ffd735
Andrea <andrea.fronzi@studio.unibo.it>
2024-11-24 22:21:11 +0100

    resulotion of bug which prevented to display the provided GUI

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -6,6 +6,8 @@ import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
@@ -16,6 +18,7 @@ import java.io.IOException;
 public final class SimpleGUI {
 
     private static final Boolean VISIBLE = true;
+    private static final int PROPORTION = 5;
 
     private final Controller controller; 
     private final JFrame frame = new JFrame();
@@ -40,14 +43,19 @@ public final class SimpleGUI {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-    public void start(){
+    public void display(){
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+        frame.setLocationByPlatform(true);
+        frame.pack();
         this.frame.setVisible(VISIBLE);
     }
 
 
     public static void main(String[] args){
-        final SimpleGUI view = new SimpleGUI();
-        view.start();
+        new SimpleGUI().display();
     }
 
 }
