22c0bdaaed04347720c8293b23c1a0d3beb3bd7a
Andrea <andrea.fronzi@studio.unibo.it>
2024-11-24 21:12:19 +0100

    implementation of Controller and SimpleGUI

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,48 @@
 package it.unibo.mvc;
 
-
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+
+    private static final String DEFAULT_FILE_NAME = "output.txt";
+    private static final String DEFAULT_PATH =
+        System.getProperty("user.home") +
+        System.getProperty("file.separator") +
+        DEFAULT_FILE_NAME;
+
+    private File currentFile;
+
+    public Controller(){
+        this.currentFile = new File(DEFAULT_PATH);
+    }
+
+    public void setCurrentFile(final File file){
+        this.currentFile = file;
+    }
+
+    public File getCurrentFile(){
+        return this.currentFile;
+    }
+
+    public String getPath(){
+        return this.currentFile.toPath().toString();
+    }
+
+    public void setFileContent(final String content) throws IOException{
+        try (final DataOutputStream outputFile = new DataOutputStream(
+            new FileOutputStream(this.getPath()));    
+            ) {
+            outputFile.writeUTF(content);
+        } 
+    }
+
 
 }
--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,13 +1,53 @@
 package it.unibo.mvc;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 /**
  * A very simple program using a graphical interface.
  * 
  */
 public final class SimpleGUI {
 
+    private static final Boolean VISIBLE = true;
+
+    private final Controller controller; 
     private final JFrame frame = new JFrame();
+
+    public SimpleGUI(){
+        this.controller = new Controller();
+        final JPanel canvas = new JPanel();
+        canvas.setLayout(new BorderLayout());
+        final JTextArea text = new JTextArea("Text Area");
+        canvas.add(text, BorderLayout.CENTER);
+        final JButton save = new JButton("Save");
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                try {
+                    controller.setFileContent(text.getText());
+                } catch (IOException e) {}
+            }
+        });
+        canvas.add(save, BorderLayout.SOUTH);
+        frame.setContentPane(canvas);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void start(){
+        this.frame.setVisible(VISIBLE);
+    }
+
+
+    public static void main(String[] args){
+        final SimpleGUI view = new SimpleGUI();
+        view.start();
+    }
 
 }
