cf7c777ca64c86119aee12c3c8ca79381ffeef36
Andrea <andrea.fronzi@studio.unibo.it>
2024-11-28 23:05:35 +0100

    implementation of Interface and Controller without controlling stile of code

--- 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,33 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
- *
+ *Controller should be used to fulfill to standard output operation in a MVC pattern
  */
 public interface Controller {
 
+    /**
+     * This method shall set the next string to print
+     * @param newLine
+     * @throws NullpointerException if {@code newLine} is null
+     */
+    void setLine(String newLine);
+
+    /**
+     * 
+     * @return the next string to print
+     */
+    String getLine();
+
+    /**
+     * 
+     * @return the history of the printed Strings
+     */
+    List<String> getHistoryLine();
+
+    /**
+     * This method print the string previously setted
+     */
+    void printString();
 }
